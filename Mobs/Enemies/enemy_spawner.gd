2bdc42e1720cab48c4ccd4e232c18e60c203988c
JoshDG03 <joshua.davila1@upr.edu>
2025-02-21 23:06:25 -0400

    Enemy and ally spawn points were created and instantiated

---
+++ Mobs/Enemies/enemy_spawner.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var main = get_tree().current_scene
+
+
+var demon_scene := preload("res://Mobs/Enemies/enemy_2d.tscn")
+var spawn_points := []
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in get_children():
+		if i is Marker2D:
+			spawn_points.append(i)
+			
+
+
+func _on_timer_timeout() -> void:
+	var spawn = spawn_points[randi() % spawn_points.size()]
+	var demon = demon_scene.instantiate()
+	demon.position = spawn.position
+	main.add_child(demon)
