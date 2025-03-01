43069e31f60af8e49a42d63ad42c637d96f06a45
Saul-Caban-UPRM <93611806+Saul-Caban-UPRM@users.noreply.github.com>
2025-03-01 12:30:56 -0400

    Added health system to both player and enemy. Rework weapon attacking animation

---
+++ Mobs/EnemyMobs/Demon.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+var Demon_health = 100
+var Demon_attack = 25
+
+func get_Demon_health() -> int:
+	return Demon_health
+	
+func set_Demon_health(Damage_to_take_away) ->void:
+	Demon_health = max(0, Demon_health - Damage_to_take_away)
+
+func get_Demon_attack()-> int:
+	return Demon_attack
--- world.gd
+++ world.gd
@@ -1,8 +1,44 @@
 extends Node2D
 
-
+var attack_distance_player = 50.0
+var attack_distance_enemy = 30.0
+@onready var EnemySpawner = $EnemySpawner
+@onready var Player = $Player
 @onready var canvas_layer = $CanvasLayer 
 
+var enemy_attack_cooldowns = {}
+var player_attack_cooldown= 0.0
 func _process(delta):
+	var current_time = Time.get_ticks_msec() / 1000.0
+	if Input.is_action_just_pressed("attack") and current_time - player_attack_cooldown >= 0.5:
+		for demon in EnemySpawner.spawned_demons:
+			var distance = Player.global_position.distance_to(demon.global_position)
+			
+			if distance <= attack_distance_player:
+				print(demon.get_Demon_health())
+				demon.set_Demon_health(Player.get_player_attack())
+				print(demon.get_Demon_health())
+
+		# Set cooldown timestamp
+		player_attack_cooldown = current_time  
+	
+	var nearest_demon = null
+	var min_distance = attack_distance_enemy
+	for demon in EnemySpawner.spawned_demons:
+		var distance = Player.global_position.distance_to(demon.global_position)
+
+		if distance <= attack_distance_enemy and distance < min_distance:
+			min_distance = distance
+			nearest_demon = demon
+	
+	if nearest_demon:
+		if nearest_demon not in enemy_attack_cooldowns or current_time - enemy_attack_cooldowns[nearest_demon] >= 1.0:
+			# Only the nearest demon attacks
+			print(Player.get_player_health())
+			Player.set_player_health(nearest_demon.get_Demon_attack())
+			print(Player.get_player_health())
+
+			# Update the cooldown timer for this demon
+			enemy_attack_cooldowns[nearest_demon] = current_time
 	if Input.is_action_just_pressed("ui_m"):
 		canvas_layer.visible = !canvas_layer.visible
