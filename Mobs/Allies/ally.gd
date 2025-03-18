cc0f45957e1f594f6a7913cc383508491bf2535a
dAninAd1248 <124212726+dAninAd1248@users.noreply.github.com>
2025-03-18 17:44:56 -0400

    Reset ally speed to main

--- Mobs/Allies/ally.gd
+++ Mobs/Allies/ally.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 @onready var foreground_bar = $AnimatedSprite2D/ForegroundBar
 @onready var animated_sprite = $AnimatedSprite2D
-@export var speed: float = 25  # Adjustable enemy speed
+@export var speed: float = 50  # Adjustable
 
 var player = null
 var move_direction = Vector2.ZERO
@@ -13,19 +13,18 @@ func _ready():
 
 func _physics_process(delta):
 	if player:
-		
+
 		move_direction = (player.global_position - global_position).normalized()
 		if (player.global_position.distance_to(global_position)) < 35:
 			move_direction = Vector2.ZERO
 			animated_sprite.play("idle")
 		else:
 			animated_sprite.play("run")
-			
+
 	velocity = move_direction * speed
 	move_and_slide()
-	
+
 	if move_direction.x < 0:
 		animated_sprite.flip_h = true
 	elif move_direction.x > 0:
 		animated_sprite.flip_h = false
-		
