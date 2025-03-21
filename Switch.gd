18b0acead55759e9d04f93f1a0e56526eee38773
JahdielMontero <jahdiel.montero@upr.edu>
2025-03-20 20:40:03 -0400

    Implement Switch and Button

---
+++ Switch.gd
@@ -0,0 +1,40 @@
+extends Area2D
+
+@onready var sprite_on: Sprite2D = $switch_on_sprite  # Reference to the "on" state sprite
+@onready var sprite_off: Sprite2D = $switch_off_sprite  # Reference to the "off" state sprite
+@onready var door: Area2D = get_node("%door")
+
+var is_on = false  # Tracks whether the switch is on or off
+var player_near = false  # Tracks if the player is near the switch
+var door_in_range = false # Tracks if there is a door near the switch
+
+func _ready():
+	sprite_on.visible = false
+	pass
+
+func _input(event):
+	if player_near and event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
+		toggle_switch()
+
+func toggle_switch():
+	is_on = !is_on
+	update_switch_state()
+	if door_in_range:
+		door.toggle_door(is_on)
+
+func update_switch_state():
+	# Toggle the visibility of the sprites based on the switch's state
+	sprite_on.visible = is_on
+	sprite_off.visible = not is_on
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		player_near = true
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		player_near = false
+
+func _on_area_entered(area: Area2D) -> void:
+	if area == door:
+		door_in_range = true
---
+++ button.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+@onready var sprite_on: Sprite2D = $button_on_sprite  # Reference to the "on" state sprite
+@onready var sprite_off: Sprite2D = $button_off_sprite  # Reference to the "off" state sprite
+
+var is_on = false  # Tracks whether the switch is on or off
+var player_near = false  # Tracks if the player is near the switch
+
+func _ready():
+	pass
+
+func _input(event):
+	if player_near and event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
+		toggle_switch()
+
+func toggle_switch():
+	sprite_on.visible = true
+	sprite_off.visible = false
+	await get_tree().create_timer(0.75).timeout
+	sprite_on.visible = false
+	sprite_off.visible = true
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		player_near = true
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		player_near = false
