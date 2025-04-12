26f8a472720a017403f060680325fa74de1f799f
Saul-Caban-UPRM <93611806+Saul-Caban-UPRM@users.noreply.github.com>
2025-04-11 20:42:50 -0400

    Resetted main

--- player.gd
+++ player.gd
@@ -89,12 +89,6 @@ func start_attack():
 	#return
 
 #player's attack and health system
-func set_heal(heal) -> void:
-	if (player_health + heal) >= 100:
-		player_health = 100 
-	else:
-		player_health = max(0, player_health + heal)
-	$PlayerHealthBar.update_health(player_health)
 func get_player_health()->int:
 	return player_health
 func set_player_health(Attack_incoming)->void:
